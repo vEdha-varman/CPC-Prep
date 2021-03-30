d7631d44c9a092f7d2797a64bc9c5cf3163f2d2b
A S Vignesh <59014425+vEdha-varman@users.noreply.github.com>
2021-03-30 18:36:08 +0530

    .

--- Week 1/Union_Intersect_SortedArrays.cpp
+++ Week 1/Union_Intersect_SortedArrays.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void Union_Intersection_SortedArrays()
+{
+    vector<int> Union, Intersection;
+
+    // 
+
+    // 
+}
+
 int main()
 {
-    // 
+    int m, n;
+    cout << "\nEnter the sizes of sorted arrays: ";
+    cin >> m >> n;
+
+    int z1[m], z2[n];
+
+    cout << "\n\nEnter the values in 1st sorted array:\n";
+    for (int i = 0; i < m; cin>>z1[i++]);
+    cout << "\n\nEnter the values in 1st sorted array:\n";
+    for (int i = 0; i < n; cin>>z2[i++]);
 
     return 0;
 }
