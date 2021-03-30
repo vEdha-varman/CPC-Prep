19ee804ece4b7b1aca0c1136c49875675a57164d
A S Vignesh <59014425+vEdha-varman@users.noreply.github.com>
2021-03-30 21:29:06 +0530

    comments

--- Week 1/Union_Intersect_SortedArrays.cpp
+++ Week 1/Union_Intersect_SortedArrays.cpp
@@ -1,60 +1,63 @@
 #include <iostream>
-#include <vector>
+
+#define SIZE 10
 
 using namespace std;
 
 void Union_Intersection_SortedArrays(int z1[], int m, int z2[], int n)
 {
-	vector<int> Union, Intersection;
-	int i=0, j=0;
+	int Union[2 * SIZE], Intersection[SIZE];
+	int i, j, x, y;
+	i = j = x = y = 0;
 
-	while (i<m && j<n)
+	// Calc section
+
+	while (i < m && j < n)
 	{
 		if (z1[i] == z2[j])
 		{
-			Intersection.push_back(z1[i]);
-			Union.push_back(z2[j]);
+			Intersection[y++] = z1[i];
+			Union[x++] = z2[j];
 
-			i++;j++;
+			i++;
+			j++;
 		}
 		else if (z1[i] < z2[j])
 		{
-			Union.push_back(z1[i]);
+			Union[x++] = z1[i];
 			i++;
 		}
 		else
 		{
-			Union.push_back(z2[j]);
+			Union[x++] = z2[j];
 			j++;
 		}
 	}
-	while (j<n)
-		Union.push_back(z2[j++]);
-	while (i<m)
-		Union.push_back(z1[i++]);
+	while (j < n)
+		Union[x++] = z2[j++];
+	while (i < m)
+		Union[x++] = z1[i++];
 
+	// Output section
 	cout << "\n\n";
-	for(int k=0; k<100; cout << "=", k++);
+	for (int k = 0; k < 100; cout << "=", k++)
+		;
 
 	cout << "\n\nUnion: ";
-	for (int i = 0; i < Union.size(); i++)
-	{
-		cout << Union[i] << " ";
-	}
+	for (i = 0; i < x; cout << Union[i] << " ", i++)
+		;
 	cout << "\n\nIntersection: ";
-	for (int i = 0; i < Intersection.size(); i++)
-	{
-		cout << Intersection[i] << " ";
-	}
+	for (j = 0; j < y; cout << Intersection[j] << " ", j++)
+		;
 
 	cout << "\n\n";
-	for(int k=0; k<100; cout << "=", k++);
-
+	for (int k = 0; k < 100; cout << "=", k++)
+		;
 }
 
 int main()
 {
-	int m, n, i=0;
+	int m, n, i = 0;
 	cout << "\nEnter the sizes of sorted arrays: ";
 	cin >> m >> n;
 
@@ -66,7 +69,7 @@ int main()
 	cout << "\n\nEnter the values in 2nd sorted array:\n";
 	for (i = 0; i < n; cin >> z2[i++])
 		;
-	
+
 	Union_Intersection_SortedArrays(z1, m, z2, n);
 
 	return 0;
