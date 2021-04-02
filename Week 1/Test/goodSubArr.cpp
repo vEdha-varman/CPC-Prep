8b8f5d89aba73902efd994a0f465f0c9ec83244f
A S Vignesh <59014425+vEdha-varman@users.noreply.github.com>
2021-04-02 18:31:21 +0530

    segment fault pending

--- Week 1/Test/goodSubArr.cpp
+++ Week 1/Test/goodSubArr.cpp
@@ -1,30 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int goodSubArr(int z[], int n, int m)
+int goodSubArr(int z[], int n, long m)
 {
-	int num=0, dp[n][n];
-	for (int i = 0; i < n; dp[0][i]=z[i],i++)
-		if (z[i] >= m)
-			num++;
+    long num = 0, dp[n][n];
+    for (int i = 0; i < n; dp[0][i] = z[i], i++)
+        if (z[i] >= m)
+            num++;
 
-	// 
-	for (int i = 1, j; i < n; i++)
-	{
-		
-		for (j = 0; j < n-i; i++)
-			dp[i][j] = dp[i-1][j] + z[j+1];
-		
-		if (dp[i][j] >= m)
-			num++;
-		
-	}
+    //
+    for (int i = 1, j; i < n; i++)
+    {
 
-	return num;
+        for (j = 0; j < n - i; j++)
+        {
+            dp[i][j] = dp[i - 1][j] + z[j + i];
+            // cout << i << ":" << j << "::" << dp[i][j] << " ";
+            if (dp[i][j] >= m)
+                num++;
+        }
+
+        // cout << "\n";
+    }
+
+    return num;
 }
 
 int main()
 {
-	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
-	return 0;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n; long m;
+    cin>>n>>m;
+    int z[n];
+    for (int i = 0; i < n; cin>>z[i],i++);
+    
+
+    cout << goodSubArr(z, n, m);
+    return 0;
 }
