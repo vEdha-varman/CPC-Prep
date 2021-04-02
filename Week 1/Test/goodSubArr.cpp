1e4a6964a5a361d93b37086e3c4f0cbf887b9614
A S Vignesh <59014425+vEdha-varman@users.noreply.github.com>
2021-04-02 17:09:47 +0530

    fst

--- Week 1/Test/goodSubArr.cpp
+++ Week 1/Test/goodSubArr.cpp
@@ -1,12 +1,30 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
+int goodSubArr(int z[], int n, int m)
+{
+	int num=0, dp[n][n];
+	for (int i = 0; i < n; dp[0][i]=z[i],i++)
+		if (z[i] >= m)
+			num++;
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    return 0;
+	// 
+	for (int i = 1, j; i < n; i++)
+	{
+		
+		for (j = 0; j < n-i; i++)
+			dp[i][j] = dp[i-1][j] + z[j+1];
+		
+		if (dp[i][j] >= m)
+			num++;
+		
+	}
+
+	return num;
+}
+
+int main()
+{
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	return 0;
 }
--- Week 1/Test/hdiff.cpp
+++ Week 1/Test/hdiff.cpp
@@ -1,43 +1,46 @@
 #include <iostream>
 using namespace std;
 
-void InSort(int Z[],int n){
-    for(int i=1,j,temp;i<n;i++){
-        temp=Z[i];
-        for(j=i-1;j>=0 && temp<Z[j];j--)
-            Z[j+1]=Z[j];
-        Z[j+1]=temp;
-        // for(j=0;j<n;j++)
-        //     cout<<Z[j]<<' ';
-        // cout<<endl;    
-    }
+void InSort(int Z[], int n)
+{
+	for (int i = 1, j, temp; i < n; i++)
+	{
+		temp = Z[i];
+		for (j = i - 1; j >= 0 && temp < Z[j]; j--)
+			Z[j + 1] = Z[j];
+		Z[j + 1] = temp;
+		// for(j=0;j<n;j++)
+		//     cout<<Z[j]<<' ';
+		// cout<<endl;
+	}
 }
 
 int Loss(int z[], int n, int m)
 {
-    InSort(z, n);
-    int min=z[m-1]-z[0];
-    
-    for(int i=1; i < n-m; i++)
-    {
-        cout << z[i+m-1] <<"-"<< z[i] <<":";
-        if(min > z[i+m-1]-z[i])
-        {
-            min = z[i+m-1]-z[i];
-        }
-    }
-    return min;
+	InSort(z, n);
+	int min = z[m - 1] - z[0];
+
+	for (int i = 1; i < n - m; i++)
+	{
+		cout << z[i + m - 1] << "-" << z[i] << ":";
+		if (min > z[i + m - 1] - z[i])
+		{
+			min = z[i + m - 1] - z[i];
+		}
+	}
+	return min;
 }
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    int n,m;
-    cin >> n >>m;
-    int z[n];
-    for(int i=0; i<n; i++)
-        cin >> z[i];
-    
-    cout<<Loss(z, m,n);
-    
-    return 0;
+int main()
+{
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	int n, m;
+	cin >> n >> m;
+	int z[n];
+	for (int i = 0; i < n; i++)
+		cin >> z[i];
+
+	cout << Loss(z, m, n);
+
+	return 0;
 }
